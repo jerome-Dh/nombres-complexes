18a8d27188668ce6badcefc00f944cfb944ee996
Jerome <jdieuhou@yahoo.fr>
2019-07-07 15:45:32 +0100

    Classe for exception

---
+++ src/main/java/projets/jeromedh/cm/complexe/ComplexeException.java
@@ -0,0 +1,32 @@
+package projets.jeromedh.cm.complexe;
+
+/**
+ * Exception de la manipulation des nombres complexe
+ *
+ * <p>
+ * 	Cette classe gère les exceptions survenues lors
+ * 	de la manipulation des nombres complexe
+ * </p>
+ *
+ * @version 1.0
+ *
+ * @date 06/07/2019
+ *
+ * @author Jerome Dh
+ *
+ */
+public class ComplexeException extends Exception
+{
+
+	/**
+	 * Constructeur avec paramètres
+	 *
+	 * @param String message
+	 */
+	public ComplexeException(String message)
+	{
+		super(message);
+	}
+		
+	
+}
